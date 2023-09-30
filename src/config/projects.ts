f5285bc6b1e07fa708a49f8fab813d3f2a13ab26
Maurice Schorn <pxlmo.codes@gmail.com>
2023-09-30 20:40:35 +0200

    added project thumbnails

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -6,31 +6,31 @@ const projects: ImageLink[] = [
   {
     name: "SpelerLevels",
     description: "A custom leveling system for Minecraft",
-    image: import("@assets/projects/SpelerLevels.png"),
+    image: import("@assets/projects/spelerlevels.png"),
     url: "/projects/SpelerLevels"
   },
   {
     name: "SpelerWarps",
     description: "A Player-managed warp plugin for Minecraft",
-    image: import("../assets/368x207.svg"),
+    image: import("@assets/projects/spelerwarps.png"),
     url: "/projects/SpelerWarps"
   },
   {
     name: "IdleWatch",
     description: "Busy? Watch others play in the meantime", // nice for afk streamers maybe
-    image: import("../assets/368x207.svg"),
+    image: import("@assets/projects/idlewatch.png"),
     url: "/projects/IdleWatch"
   },
   {
     name: "Boosters",
     description: "Boost xp gain and shop sell for everyone",
-    image: import("../assets/368x207.svg"),
+    image: import("@assets/projects/boosters.png"),
     url: "/projects/Boosters"
   },
   {
     name: "PaidPortals",
     description: "Unlock dimensions by donating to a money pool",
-    image: import("../assets/368x207.svg"),
+    image: import("@assets/projects/paidportals.png"),
     url: "/projects/PaidPortals"
   }
 ];
