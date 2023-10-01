ad1e1420b6a547eb288cab5eeffae8053a79c9ce
Maurice Schorn <pxlmo.codes@gmail.com>
2023-10-01 14:15:59 +0200

    changed descriptions and added redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/discord": "https://discordapp.com/users/398509167351955456/",
+    "/github": "https://github.com/maurxce/",
+    "/mail": "mailto:contact@schorn.md"
+  }
+});
