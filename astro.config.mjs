4bb9739a729399a63fae0f5a3d32177806909acb
Maurice Schorn <pxlmo.codes@gmail.com>
2023-10-01 17:21:05 +0200

    added purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://schorn.md",
   redirects: {
     "/discord": "https://discordapp.com/users/398509167351955456/",
     "/github": "https://github.com/maurxce/",
     "/mail": "mailto:contact@schorn.md"
-  }
+  },
+  integrations: [
+    robotsTxt({
+      sitemap: false,
+      host: "schorn.md"
+    }),
+    purgecss()
+  ]
 });
