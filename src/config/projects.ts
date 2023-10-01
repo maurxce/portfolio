4c1f767201248d6f718c44af2f08fc6ac189336a
Maurice Schorn <pxlmo.codes@gmail.com>
2023-10-01 10:50:10 +0200

    switching to ImageMetadata

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,4 +1,5 @@
 import type { ImageLink } from "src/common/interface";
+import type { ImageMetadata } from "astro";
 // @TODO: add BitLib, BedrockManager?
 // @TODO sort by tag?
 
@@ -6,30 +7,35 @@ const projects: ImageLink[] = [
   {
     name: "SpelerLevels",
     description: "A custom leveling system for Minecraft",
-    image: import("@assets/projects/spelerlevels.png"),
+    // @ts-ignore
+    image: import("@assets/projects/spelerlevels.png"), 
     url: "/projects/SpelerLevels"
   },
   {
     name: "SpelerWarps",
     description: "A Player-managed warp plugin for Minecraft",
+    // @ts-ignore
     image: import("@assets/projects/spelerwarps.png"),
     url: "/projects/SpelerWarps"
   },
   {
     name: "IdleWatch",
     description: "Busy? Watch others play in the meantime", // nice for afk streamers maybe
+    // @ts-ignore
     image: import("@assets/projects/idlewatch.png"),
     url: "/projects/IdleWatch"
   },
   {
     name: "Boosters",
     description: "Boost xp gain and shop sell for everyone",
+    // @ts-ignore
     image: import("@assets/projects/boosters.png"),
     url: "/projects/Boosters"
   },
   {
     name: "PaidPortals",
     description: "Unlock dimensions by donating to a money pool",
+    // @ts-ignore
     image: import("@assets/projects/paidportals.png"),
     url: "/projects/PaidPortals"
   }
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="astro/client" />
+
+declare module '*.png' {
+  const metadata: ImageMetadata;
+  export default metadata;
+}
