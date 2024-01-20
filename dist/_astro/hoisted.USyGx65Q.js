a5b88f6f0ef0e0c09117f832e41f4f01609b0a5f
Maurice Schorn <pxlmo.codes@gmail.com>
2024-01-20 22:05:32 +0100

    year calc is now happening client side

---
+++ dist/_astro/hoisted.USyGx65Q.js
@@ -0,0 +1,1 @@
+const e=document.querySelector("p.year");e.innerHTML=`${new Date().getFullYear()}`;
---
+++ dist/_astro/profile_picture.oHESsnhS.js
@@ -0,0 +1,1 @@
+const p={src:"/_astro/profile_picture.bwhp4ipr.png",width:600,height:600,format:"png"};export{p as default};
